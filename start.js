7fe0ac9c5001b08eed6baf6176892cc05a0561f8
Jebril <dontlikemail@gmx.de>
2022-03-23 09:33:08 +0100

    Update start.js

--- start.js
+++ start.js
@@ -1,5 +1,6 @@
 /** @param {NS} ns **/
 export async function main(ns) {
+	ns.disableLog('ALL')
 	var servers =[]
 	var files = ['grow.script', 'weak.script', 'hack.script']
 	await ns.write(files[0], 'grow(args)', 'w')
@@ -12,15 +13,18 @@ export async function main(ns) {
 				var tmax = Math.floor((ns.getServerMaxRam(server)-ns.getServerUsedRam(server))/1.7)
 				if (tmax > 0 && ns.getServerNumPortsRequired(server)==0) {
 					ns.nuke(server)
-					ns.exec(files[2], server, tmax,'n00dles')
+					ns.exec(files[2], server, tmax, 'n00dles')
+					ns.print(server + ': ' + tmax+ ' / ' + ns.getServerNumPortsRequired(server))
 				}
 				await scanServers(current, server)
 			}
 		}
 	}
 	while(true) {
+		ns.clearLog()
 		await scanServers('', 'home')
 		ns.print(ns.getServerMoneyAvailable('n00dles'))
+		ns.print(ns.getServerSecurityLevel('n00dles'))
 		await ns.asleep(1000)
 	}
 }
