134d393ed52511bdb27c7a7360c259f98ceaa946
Jebril <dontlikemail@gmx.de>
2022-03-25 23:23:19 +0100

    Update ast.js

--- ast.js
+++ ast.js
@@ -4,10 +4,13 @@ export async function main(ns) {
 	let singlvl = 1
 	let nram = Math.pow(2, 7-2*singlvl)
 	if (ns.getServerMaxRam('home') > nram ){
-		ns.scriptKill('jeb.js', 'home')
-		ns.run('ator.js')
-		ns.run('abuy.js')
-		ns.run('jeb.js')
+		ns.run('btor.js')
+		if (!ns.fileExists('tor.txt')){
+			ns.run('ator.js')
+		}
+		else {
+			ns.run('abuy.js')
+		}
 	}
 	else {
 		ns.tprint('Need ' + 2*nram + ' Ram!')
