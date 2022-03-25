63b525f77160c61c19ae97caa56f5407ba12d828
Jebril <dontlikemail@gmx.de>
2022-03-25 23:37:28 +0100

    Update ator.js

--- ator.js
+++ ator.js
@@ -1,8 +1,8 @@
 /** @param {NS} ns **/
 export async function main(ns) {
   while(!ns.purchaseTor()) {
+    await ns.asleep(1000)
   }
   ns.tprint('Aquired Tor!')
   await ns.write('tor.txt', '', 'w')
-  await ns.asleep(1000)
 }
