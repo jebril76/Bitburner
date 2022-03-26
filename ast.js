67b503f50cb8688f55ae147037ae7ece53c8a9e2
Jebril <dontlikemail@gmx.de>
2022-03-26 13:17:29 +0100

    Update ast.js

--- ast.js
+++ ast.js
@@ -1,10 +1,16 @@
 /** @param {NS} ns **/
 export async function main(ns) {
-	let singlvl = ns.getOwnedSourceFiles().find(bitnode => bitnode.n === 4).lvl
-	//let singlvl = 1
+	//let singlvl = ns.getOwnedSourceFiles().find(bitnode => bitnode.n === 4).lvl
+	let singlvl = 1
+	let exes = ['brutessh', 'ftpcrack', 'relaysmtp', 'httpworm', 'sqlinject', 'DeepscanV1', 'DeepscanV2', 'Autolink', 'ServerProfiler']
+	let tor=false
 	let nram = Math.pow(2, 7-2*singlvl)
 	if (ns.getServerMaxRam('home') > nram ){
-		if (!ns.fileExists('BruteSSH.exe')){
+		i = 0
+		while (ns.fileExists(exes[i] + '.exe')){
+			i++
+		}
+		if (i<exes.length-1){
 			ns.tprint('AutoTor started!')
 			ns.run('ator.js')
 			while(ns.getRunningScript('ator.js')){
