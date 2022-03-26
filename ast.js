99792f20c856a11f054ad1c1e2c27f4fc0a925a3
Jebril <dontlikemail@gmx.de>
2022-03-26 21:06:46 +0100

    Update ast.js

--- ast.js
+++ ast.js
@@ -1,9 +1,8 @@
 /** @param {NS} ns **/
 export async function main(ns) {
-	//let singlvl = ns.getOwnedSourceFiles().find(bitnode => bitnode.n === 4).lvl
-	let singlvl = 1
+//	let singlvl = ns.getOwnedSourceFiles().find(bitnode => bitnode.n === 4).lvl
+	let singlvl = 3
 	let exes = ['brutessh', 'ftpcrack', 'relaysmtp', 'httpworm', 'sqlinject', 'DeepscanV1', 'DeepscanV2', 'Autolink', 'ServerProfiler']
-	let tor=false
 	let nram = Math.pow(2, 7-2*singlvl)
 	if (ns.getServerMaxRam('home') > nram ){
 		let i = 0
