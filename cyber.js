2daf76c3af9c552b1392d7784e07e666e5d8897a
Jebril <dontlikemail@gmx.de>
2022-03-26 20:43:09 +0100

    Create cyber.js

---
+++ cyber.js
@@ -0,0 +1,35 @@
+/** @param {NS} ns **/
+//WIP
+export async function main(ns) {
+	let bado = ['CSEC','avmnite-02h','I.I.I.I','run4theh111z', 'The-Cave']
+	for (let bd in bado) {
+		let path = ['home']
+		let deepth = 0
+		let done=false
+		ns.connect('home')
+		async function scanServers(host, current) {
+			for (let server of ns.scan(current)) {
+				if (!done){
+					deepth++;
+					path[deepth] = server
+					ns.connect(server)
+					if (host != server) {
+						if (server == bd) {
+							ns.tprint('Backdoor to ' + bd)
+							await ns.installBackdoor()
+							done=true
+						}
+						else {
+							await scanServers(current, server)
+						}
+					}
+					if (!done){
+						deepth--;
+						ns.connect(path[deepth])
+					}
+				}
+			}
+		}
+		await scanServers('', 'home')
+	}
+}
