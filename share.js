5e638f8faa78b15b9eee2b9ee6e220226f5f9eb5
Jebril <dontlikemail@gmx.de>
2022-03-31 09:18:02 +0200

    Update share.js

--- share.js
+++ share.js
@@ -1,10 +1,10 @@
 /** @param {NS} ns **/
 export async function main(ns) {
 	var servers =[]
-	var files = ['grow.script', 'weak.script', 'hack.script']
-	await ns.write(files[0], 'grow(args)', 'w')
-	await ns.write(files[1], 'weaken(args)', 'w')
-	await ns.write(files[2], 'hack(args)', 'w')
+	var files = ['grow.js', 'weak.js', 'hack.js']
+    	await ns.write(files[0], '/** @param {NS} ns **/ export async function main(ns) {await ns.grow(ns.args[0])}', 'w')
+    	await ns.write(files[1], '/** @param {NS} ns **/ export async function main(ns) {await ns.weaken(ns.args[0])}', 'w')
+   	await ns.write(files[2], '/** @param {NS} ns **/ export async function main(ns) {await ns.hack(ns.args[0])}', 'w')
 	async function scanServers(host, current) {
 		for (let server of ns.scan(current)) {
 			if (host != server) {
